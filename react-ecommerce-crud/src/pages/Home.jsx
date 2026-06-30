5cfe60b0c962e85bdb7f9ad82757b6d395461c2d
Aleena Bijoy <aleenabijoy04@gmail.com>
2026-06-30 17:18:14 +0530

    Fetch products from Fake Store API with loading and error handling

--- react-ecommerce-crud/src/pages/Home.jsx
+++ react-ecommerce-crud/src/pages/Home.jsx
@@ -1,4 +1,35 @@
+import {useEffect,useState} from "react";
+import {getProducts} from "../services/api";
+
 function Home(){
-    return <h1>Home Page</h1>;
+    const[products,setProducts]=useState([]);
+    const [loading,setLoading]=useState(true);
+    const [error,setError]=useState("")
+    useEffect(() =>{
+        async function fetchProducts() {
+            
+    
+            try{
+                const data =await getProducts();
+                setProducts(data);
+            }catch(err){
+                setError(err.message);
+            }finally{
+                setLoading(false);
+            }
+        }
+        fetchProducts();
+        }, []);
+    return (
+  <div className="container mt-4">
+    <h1>Products</h1>
+
+    {loading && <p>Loading...</p>}
+
+    {error && <p>{error}</p>}
+
+    <pre>{JSON.stringify(products, null, 2)}</pre>
+  </div>
+);
 }
 export default Home;
